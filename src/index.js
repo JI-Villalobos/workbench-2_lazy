bd7ebda514f4f3e785c1a940bd5d83b5f34ab0d2
Chuy <chuy12villalobos@gmail.com>
2021-05-07 20:07:46 -0500

    [ADD] js upload images

--- src/index.js
+++ src/index.js
@@ -3,4 +3,43 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//create (1) image
+
+/* 
+<div class="p-4">
+    <img class="mx-auto" width="320" src="https://randomfox.ca/images/2.jpg" alt="">
+</div>
+*/
+
+const maximum = 122
+const minimum = 1
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum
+
+const createImageNode = () => {
+    const container = document.createElement('div')
+    container.className = "p-4"
+
+    const image = document.createElement('img')
+    image.className = 'mx-auto'
+    image.width = '320'
+    image.src = `https://randomfox.ca/images/${random()}.jpg` //TODO  
+
+    container.appendChild(image)
+
+
+    return container
+}
+
+const newImage = createImageNode()
+
+const mountNode =  document.getElementById('images')
+
+const addButton = document.querySelector('button')
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage)
+}
+
+addButton.addEventListener("click", addImage)
+
+
